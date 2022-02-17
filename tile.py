d6da9ef8c33612923ebdccf02cf5e0f3343e97c5
TaylorWhite21 <82013462+TaylorWhite21@users.noreply.github.com>
2022-02-16 22:05:11 -0500

    Add files via upload

--- tile.py
+++ tile.py
@@ -1,10 +1,14 @@
 import pygame 
 from settings import * 
 
+
 class Tile(pygame.sprite.Sprite): 
     def __init__(self,pos,groups):
+        # Super is used to give access to methods and properties
         super().__init__(groups) 
+        # Assigns the rock image
         self.image = pygame.image.load('./graphics/rock.png').convert_alpha()
+        
         self.rect = self.image.get_rect(topleft = pos) 
 
     
