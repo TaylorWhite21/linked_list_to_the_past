1ca1986f7e40e3bc2a9c0d0cab7c97638547cf17
Bionca Bond <bioncab@zillowgroup.com>
2022-02-18 10:19:33 -0800

    no errors with the weapons implementation, only errors are related to the animation work.

--- settings.py
+++ settings.py
@@ -6,8 +6,8 @@ TILESIZE = 64
 
 #weapon
 weapon_data = { 
-'punch': {'cooldown':100, 'damage': 15, 'graphic': filepath}
-#'fire' : {'cooldown':100, 'damage': 15, 'graphic': filepath}
+'sword': {'cooldown':100, 'damage': 15, 'graphic': '../graphics/weapons/punch/punch.png'}
+# 'fire' : {'cooldown':400, 'damage': 30, 'graphic': filepath}
 
 
 }
