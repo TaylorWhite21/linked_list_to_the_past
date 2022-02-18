8c9d34822e3e5337d1cb9b668cea6c403fb71f0c
Bionca Bond <bioncab@zillowgroup.com>
2022-02-18 10:23:13 -0800

    image tweak for weapon. added full_temp

--- settings.py
+++ settings.py
@@ -6,8 +6,7 @@ TILESIZE = 64
 
 #weapon
 weapon_data = { 
-'sword': {'cooldown':100, 'damage': 15, 'graphic': '../graphics/weapons/punch/punch.png'}
+'sword': {'cooldown':100, 'damage': 15, 'graphic': '../graphics/weapons/sword/full_temp.png'}
 # 'fire' : {'cooldown':400, 'damage': 30, 'graphic': filepath}
-
 
 }
