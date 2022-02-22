2c4bb9e774b0d5e0768ff586d3f3c5ecef0d0124
micgreene <mgreene11111@yahoo.com>
2022-02-21 20:05:57 -0800

    updating

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
-import pygame, sys
-from settings import *
+
+# from settings import *
 # from debug import debug
 import os
-os.environ['SDL_VIDEODRIVER']='dummy'
-os.environ["SDL_AUDIODRIVER"]="dummy"
+
+# try:
+#   os.environ["Display"]
+# except:
+#   os.environ['SDL_VIDEODRIVER']='directx'
+#   os.environ['SDL_AUDIODRIVER'] = 'dsp'
+# # os.environ["SDL_AUDIODRIVER"]="dummy"
+import pygame, sys
+pygame.init()
 
 class Game:
   def __init__(self):
-    pygame.init()
-    
-    self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.init()    
+    self.screen = pygame.display.set_mode((800,600))
     self.clock = pygame.time.Clock()
+
   def run(self):
     while True:
+      print(pygame.display.update)
+      self.screen.fill((252,0,0))
+      pygame.display.update()   
+      # self.screen.fill('black')
+      # pygame.display.update()
+      self.clock.tick(60)
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           pygame.quit()
           sys.exit()
-          
-      self.screen.fill('black')
-      pygame.display.update()
-      self.clock.tick(FPS)
 
 if __name__ == '__main__':
   game = Game()
